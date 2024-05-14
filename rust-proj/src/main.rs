bc58b968e0484004c40c47701a1910b0d5923ee8
Shubham Gupta <shubham272101@gmail.com>
2024-05-14 20:51:27 +0530

    update main.rs

--- rust-proj/src/main.rs
+++ rust-proj/src/main.rs
@@ -4,11 +4,22 @@
 //     print!("Full name is : {} {}",first_name,last_name);
 // }
 
+// 
+// fn main(){
+//     let num1 : i8 = 5;
+//     let num2 : i8 = 114;
+//     print!("sum of {} and {} is {}",num1,num2,num1+num2);
+// }
 
+
+// this wil cause error during compilation which states add with overfloww
 fn main(){
-    let num1 : i8 = 5;
-    let num2 : i8 = 114;
-    print!("sum of {} and {} is {}",num1,num2,num1+num2);
+    let mut number : i8 = 5;  //here we introduce new thing 'mut' which is for mutable and let compiler to change value of a variabke during run time
+    for i in 0..1000{
+        number = number + 100;
+    }
+
+    print!("value of number is  : {}",number);
 }
 
 
